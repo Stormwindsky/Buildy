f7efd8331461299c560c7a6bddab30cf30eee548
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-05 20:01:34 -0400

    Add files via upload

--- setup_suite.sh
+++ setup_suite.sh
@@ -6,15 +6,28 @@ NC='\033[0m'
 
 echo -e "${BLUE}--- Buildy Suite: Installation (Python + Node.js) ---${NC}"
 
-# 1. Dépendances système
+# 1. Gestion du fichier .gitignore
+if [ ! -f .gitignore ]; then
+    echo -e "${BLUE}[0/2] Création du fichier .gitignore...${NC}"
+    cat <<EOF > .gitignore
+env/
+node_modules/
+LICENSE
+EOF
+    echo -e "${GREEN}Fichier .gitignore créé avec succès.${NC}"
+else
+    echo -e "${BLUE}[0/2] Le fichier .gitignore existe déjà, étape ignorée.${NC}"
+fi
+
+# 2. Dépendances système
 sudo apt update
 sudo apt install -y nodejs npm python3-venv
 
-# 2. Installation Electron (pour l'option 4)
+# 3. Installation Electron (pour l'option 4)
 echo -e "${BLUE}[1/2] Installation des modules Node.js...${NC}"
 npm install
 
-# 3. Installation Python (pour options 1, 2, 3)
+# 4. Installation Python (pour options 1, 2, 3)
 echo -e "${BLUE}[2/2] Configuration de l'environnement Python...${NC}"
 rm -rf env
 python3 -m venv --system-site-packages env
